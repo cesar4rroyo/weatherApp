12ef9fd0cb4fdffc450c843c60c0e6df28c4c346
Cesar Arroyo <cesardavid_09@hotmail.com>
2020-03-04 15:35:50 -0500

    Updates

---
+++ precache-manifest.6615060037033d306c7515ec724c7a4e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed53f6f602034dd75c9cca6f3695739",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "1791bd422bcfa5c134e5",
+    "url": "/weatherApp/static/css/2.ec7e815e.chunk.css"
+  },
+  {
+    "revision": "55e66e69eb984c0abd3c",
+    "url": "/weatherApp/static/css/main.33106030.chunk.css"
+  },
+  {
+    "revision": "1791bd422bcfa5c134e5",
+    "url": "/weatherApp/static/js/2.9c298b5d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weatherApp/static/js/2.9c298b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e66e69eb984c0abd3c",
+    "url": "/weatherApp/static/js/main.6622ea8e.chunk.js"
+  },
+  {
+    "revision": "cb9103efee0b49bedf1c",
+    "url": "/weatherApp/static/js/runtime-main.15988c55.js"
+  },
+  {
+    "revision": "e622ff4e0beeba2f34c672f7ce76731a",
+    "url": "/weatherApp/static/media/clouds-dawn-nature-ocean-561463.e622ff4e.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
